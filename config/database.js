2b958658b965f21355d60fc149a1be67e969cafa
Diego <dicornyt@gmail.com>
2022-12-03 15:18:32 -0500

    Base de Datos

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'mysql',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      host: env('DATABASE_HOST', '216.246.46.82'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'qaolnfcq_ecreea_strapi'),
+      user: env('DATABASE_USERNAME', 'qaolnfcq_ecreea_ti'),
+      password: env('DATABASE_PASSWORD', 'OS;)kaLL-ghG'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
+    debug: false,
   },
 });
